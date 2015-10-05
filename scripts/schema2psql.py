d896238ca7857013b1f99f10dbe60637a9b228cd
Ryan Sturmer <ryansturmer@gmail.com>
2015-10-05 07:05:16 -0500

    Finish schema2psql.py

--- scripts/schema2psql.py
+++ scripts/schema2psql.py
@@ -1,11 +1,27 @@
 import tableview
 import os, sys
 
+PRIMARY_KEY = 'uuid'
 infile = sys.argv[1]
 outfile = os.path.splitext(infile)[0] + '.sql'
 
 table = tableview.load(infile)
+
+
+def row_to_sql(row):
+    name = row[0].lower()
+    cols = row[1:]
+    primary_key = PRIMARY_KEY
+    retval = ['CREATE TABLE %s {' % name]
+    retval.append('\tPRIMARY KEY(%s)' % primary_key)
+    for col in cols:
+        if(col != ""):
+            col = col.lower().replace(' ', '_')
+            retval.append('\t"%s" text,' % col)
+    retval[-1].rstrip(',')
+    retval.append('};')
+    return '\n'.join(retval)
+
+
 for row in table:
-    table_name = row[0]
-    columns = row[1:]
-    print table_name
+    print row_to_sql(row)
